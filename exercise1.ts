d2163f68c7f9c4b92ea34bf6e54d7f30f346b365
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-11 01:32:08 +0500

    update

---
+++ exercise1.ts
@@ -0,0 +1,1 @@
+console.log("Hello World, Thank you Zia Sir")
---
+++ exercise2.ts
@@ -0,0 +1,6 @@
+// Exsersise 2: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+
+let personName: string = "Kamran"
+let message: string = `Hello ${personName}, hope you are doing will these days.`
+console.log(message)
