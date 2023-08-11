915a912c0e4c07d9bcf88b5ccaac79706e560d70
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-12 00:21:21 +0500

    update

---
+++ exercise6.js
@@ -0,0 +1,6 @@
+"use strict";
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let personName = "\t \t \n\n Muhammad Kamran \t \n\n";
+console.log(`Name with whitespace: ${personName}`);
+let strippedName = personName.trim();
+console.log(`Stripped name: ${strippedName}`);
---
+++ exercise6.ts
@@ -0,0 +1,10 @@
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+
+let personName: string = "\t \t \n\n Muhammad Kamran \t \n\n";
+
+console.log(`Name with whitespace: ${personName}`)
+
+let strippedName: string = personName.trim()
+
+console.log(`Stripped name: ${strippedName}`)
