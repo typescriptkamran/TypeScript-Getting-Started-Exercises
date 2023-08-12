12eca840966df53069c7401fa740bafb877977f1
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-12 12:59:15 +0500

    update 19

---
+++ exercise19.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.*/
+// Importing Guests List from exercise 15
+const exercise15_1 = require("./exercise15");
+console.log(`\nPrinting message indicating number of guests`);
+console.log(`We had finally invited ${exercise15_1.guestList.length} guest in Exercise 15.`);
---
+++ exercise19.ts
@@ -0,0 +1,8 @@
+/* Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.*/
+// Importing Guests List from exercise 15
+import{ guestList } from './exercise15'
+
+console.log(`\nPrinting message indicating number of guests`)
+console.log(`We had finally invited ${guestList.length} guest in Exercise 15.`)
+
+export{};
