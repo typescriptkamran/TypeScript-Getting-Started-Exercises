6b47d2a940524303801f7c700f4f5f3eb504f160
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-12 21:34:00 +0500

    updated 23

---
+++ exercise23.js
@@ -0,0 +1,49 @@
+"use strict";
+/*
+Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+• Tests for equality and inequality with strings
+
+• Tests using the lower case function
+
+• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+
+• Tests using "and" and "or" operators
+
+• Test whether an item is in a array
+
+• Test whether an item is not in a array
+ */
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru'); // True
+console.log("Is car == 'honda'? I predict False.");
+console.log(car == 'honda'); // False
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru'); // True
+console.log("Is car === 'Subaru'? I predict False.");
+console.log(car === 'Subaru'); // False
+console.log("Is car !== 'toyota'? I predict True.");
+console.log(car !== 'toyota'); // True
+console.log("Is car !== 'subaru'? I predict False.");
+console.log(car !== 'subaru'); // False
+console.log("Is car != 'mazda'? I predict True.");
+console.log(car != 'mazda'); // True
+console.log("Is car != 'subaru'? I predict False.");
+console.log(car != 'subaru'); // False
+let age = 25;
+console.log("Is age > 18? I predict True.");
+console.log(age > 18); // True
+console.log("Is age < 21? I predict False.");
+console.log(age < 21); // False
