0eb413dda185b0cec4ab85be6e05611e54a606a2
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-12 17:27:21 +0500

    update 21

---
+++ exercise21.js
@@ -0,0 +1,39 @@
+"use strict";
+/*
+They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const bouquet = {
+    name: "Spring Delight",
+    price: 250,
+    description: "Beautiful arrangement of spring flowers"
+};
+// define an array of objects
+let bouquets = [];
+// pushing multipul object
+bouquets.push(bouquet);
+bouquets.push({
+    name: "Summer bliss",
+    price: 350,
+    description: "beautiful arrangement of Summer flowers"
+});
+let bouquet3 = {
+    name: "Red Hot",
+    price: 350,
+    description: "Beautiful arrangement of red roses"
+};
+bouquets.push(bouquet3);
+// console.log(bouquets);
+bouquets.push({ name: "Freshness", price: 250, description: "A beautiful arrangement of white and yellow roses" });
+// console.log(bouquets);
+// function to display bouquets
+function displayBouquets(bouquets) {
+    for (let i of bouquets) {
+        console.log(`
+        ${i.name}
+        ${i.description}
+        price: ${i.price} USD
+        ______________\n`);
+    }
+}
+displayBouquets(bouquets);
