cfcd7f5f1d1eb12754e76e3608ccb0171bdcc0f5
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-12 23:10:36 +0500

    update ex26

--- exercise26.js
+++ exercise26.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /*
 Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
 • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
@@ -8,7 +9,7 @@ Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and writ
 • Write one version of this program that runs the if block and another that runs the else block.
 */
 // Version 1 (runs the if block)
-Object.defineProperty(exports, "__esModule", { value: true });
+console.log(`\n1st version of code`);
 let alien_color = 'green';
 if (alien_color === 'green') {
     console.log('Congratulations! You just earned 5 points for shooting the alien.');
@@ -17,6 +18,7 @@ else {
     console.log('Congratulations! You just earned 10 points for shooting the alien.');
 }
 // version 2 ( runs the else block.) 
+console.log(`\n2nd version of code`);
 alien_color = 'blue';
 if (alien_color === 'green') {
     console.log('Congratulations! You just earned 5 points for shooting the alien.');
