44b0e9444b765bd2aa33812b197bc56e29b8feec
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-12 22:14:53 +0500

    update

--- exercise23.js
+++ exercise23.js
@@ -11,20 +11,8 @@ console.log(car == 'subaru')
 • Look closely at your results, and make sure you understand why each line evaluates to True or False.
 
 • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
-
-More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
-• Tests for equality and inequality with strings
-
-• Tests using the lower case function
-
-• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
-
-• Tests using "and" and "or" operators
-
-• Test whether an item is in a array
-
-• Test whether an item is not in a array
  */
+Object.defineProperty(exports, "__esModule", { value: true });
 let car = 'subaru';
 console.log("Is car == 'subaru'? I predict True.");
 console.log(car == 'subaru'); // True
--- exercise23.ts
+++ exercise23.ts
@@ -10,19 +10,6 @@ console.log(car == 'subaru')
 • Look closely at your results, and make sure you understand why each line evaluates to True or False.
 
 • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
-
-More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
-• Tests for equality and inequality with strings
-
-• Tests using the lower case function
-
-• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
-
-• Tests using "and" and "or" operators
-
-• Test whether an item is in a array
-
-• Test whether an item is not in a array
  */
 
 let car = 'subaru';
@@ -58,3 +45,4 @@ console.log(age > 18); // True
 console.log("Is age < 21? I predict False.");
 console.log(age < 21); // False
 
+export{};
