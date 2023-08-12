a62bab165f91a35dec68ad9af36967016de7208d
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-12 10:02:29 +0500

    updated 10 and 9

---
+++ exercise9.ts
@@ -0,0 +1,10 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoriteNumber: number = 7
+
+let message: string = `My favorit number is ${favoriteNumber}`;
+
+
+console.log(message)
+
+export{};
