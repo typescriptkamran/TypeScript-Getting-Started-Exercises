40a68d429cee3f83598bc4c4cdf2fbf006f230f2
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-17 01:56:38 +0500

    update ex 30

--- exercise30.ts
+++ exercise30.ts
@@ -1,4 +1,4 @@
-/* Hello Admin: Make a array of five or more userNames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+/* Hello Admin: Make an array of five or more userNames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
 
 • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 
@@ -16,5 +16,4 @@ function greetUsers(): void {
         }
     }
 }
-
 greetUsers()
