576a389f3e6c3b095b196a7f81feead34dd8f5e7
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-16 23:49:05 +0500

    update 29

---
+++ exercise29.ts
@@ -0,0 +1,28 @@
+/* Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+• Make a array of your three favorite fruits and call it favorite_fruits.
+
+• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas! */
+
+// Define the array of fruits
+const favoriteFruits: string[] = ["apple", "banana", "orange", "grape", "mango"];
+
+// Check for specific fruits using independent if statements
+if (favoriteFruits.includes("apple")) {
+    console.log("I like apples!");
+}
+
+if (favoriteFruits.includes("banana")) {
+    console.log("I enjoy bananas!");
+}
+
+if (favoriteFruits.includes("orange")) {
+    console.log("Oranges are delicious!");
+}
+
+if (favoriteFruits.includes("grape")) {
+    console.log("Grapes are tasty!");
+}
+
+if (favoriteFruits.includes("mango")) {
+    console.log("Mangoes are one of my favorites!");
+}
