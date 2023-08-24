1fa7e39969746533d706705f51d05b6ead905068
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-24 22:23:16 +0500

    rename files to show in correct order

--- dist/exercise9.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-Object.defineProperty(exports, "__esModule", { value: true });
-let favoriteNumber = 7;
-let message = `My favorit number is ${favoriteNumber}`;
-console.log(message);
